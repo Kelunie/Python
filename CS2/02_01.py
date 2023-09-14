b14d096fc00626b5dfd5222ccc4a44a1b5ea0d26
itachirr <crodriguezco@est.utn.ac.cr>
2023-09-14 05:17:49 -0600

    Clases y sus cosas con practica

---
+++ CS2/02_01.py
@@ -0,0 +1,37 @@
+from Heroe import hero
+
+# creamos un metodo para ver la informacion de la clase
+
+
+def verHero(lstHeroe):
+    i = 1
+    for her in lstHeroe:
+        print('El hero {0} se llama {1} el nombre, su abtributo es {2} de sus habilidades son: {3}, {4}, {5}, {6} '.format
+              (i, her.getNombre(), her.getAtrubuto(), her.getSpell1(), her.getSpell2(), her.getSpell3(), her.getSpell4()))
+        i += 1
+    print('')
+
+
+# creamos la instancia de la clase
+her1 = hero()
+
+# Colocamos los valores con set
+her1.setNombre('Arc Warden')
+her1.setAtributo('Agilidad')
+her1.setSpell1('Flujo')
+her1.setSpell2('Campo Magnetico')
+her1.setSpell3('Espectro Centellante')
+her1.setSpell4('Duplicado de la Tempestad')
+
+
+print('El Heroe se llama {0}'.format(her1.getNombre()))
+print('Su atributo es {0}'.format(her1.getAtributo()))
+print('Sus habilidades son:')
+print(her1.getSpell1())
+print(her1.getSpell2())
+print(her1.getSpell3())
+print(her1.getSpell4())
+
+
+# Practica #1 Crea una clase con un constructor HeroesV2 con sus get y sett y agregue una lista de heroes y utilice
+# el contructor dado def verHero(lstHero):'
---
+++ CS2/Heroe.py
@@ -0,0 +1,44 @@
+class hero:
+    _nombre = ''
+    _atributo = ''
+    _spell1 = ''
+    _spell2 = ''
+    _spell3 = ''
+    _spell4 = ''
+
+    # get / Set de python
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getNombre(self):
+        return self._nombre
+
+    def setAtributo(self, atributo):
+        self._atributo = atributo
+
+    def getAtributo(self):
+        return self._atributo
+
+    def setSpell1(self, spell1):
+        self._spell1 = spell1
+
+    def getSpell1(self):
+        return self._spell1
+
+    def setSpell2(self, spell2):
+        self._spell2 = spell2
+
+    def getSpell2(self):
+        return self._spell2
+
+    def setSpell3(self, spell3):
+        self._spell3 = spell3
+
+    def getSpell3(self):
+        return self._spell3
+
+    def setSpell4(self, spell4):
+        self._spell4 = spell4
+
+    def getSpell4(self):
+        return self._spell4
